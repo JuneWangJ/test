3442ecce5479a6f3bdce7b835b3ba442fb5ecee1
胡哲 <286808713@qq.com>
2015-05-18 20:43:36 +0800

    图片修改

--- js/course.js
+++ js/course.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
 	$("#btn-start").click(function() {
 		$('#iframe3');
 		var url = window.location.href;
-		$("#iframe3").attr('src',"http://"+url.split("/")[2]+":6080/vnc_auto.html");
+		$("#iframe3").attr('src',"http://"+url.split("/")[2]+":6080/vnc.html");
 	});
 
 	$("#btn-back").click(function(){
